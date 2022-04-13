eb7016e53c52e39024b08e42b90e016defd48ef4
BOBBIJDJ <bobbijdj23@outlook.com>
2022-04-13 19:41:39 +0200

    added a file to emit redstone signals

---
+++ redston_receive.lua
@@ -0,0 +1,30 @@
+local component = require("component")
+local sides = require("sides")
+local event = require("event")
+
+local receive = event.pull
+local open_port = component.modem.open
+local redstone = component.redstone
+local temp0, temp1, temp2, temp3, temp4, received_message
+
+print("to what side do you want to emit redstone?")
+
+print("from what port do you want to receive data?")
+
+local port = tonumber(io.read())
+
+open_port(port)
+
+while (true) do
+
+    temp0, temp1, temp2, temp3, temp4, received_message = receive("modem")
+    
+    if (received_message == "call") then
+        
+        redstone.setOutput(sides.right, 15)
+
+        redstone.setOutput(sides.right, 0)
+
+    end
+
+end
